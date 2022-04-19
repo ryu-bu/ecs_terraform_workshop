0f859b260d4b2f528f96651d004ec44665901bed
georgemjohnson11 <georgemjohnson11@gmail.com>
2022-04-19 17:13:23 -0600

    Add registries for other containerized software

--- ecr.tf
+++ ecr.tf
@@ -66,3 +66,179 @@ resource "aws_ecr_lifecycle_policy" "constellationjs_repo_policy" {
 }
 EOF
 }
+
+resource "aws_ecr_repository" "fluigicloud_repo" {
+  name                 = "fluigicloud"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "gjohnson"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "fluigicloud_repo_policy" {
+  repository = aws_ecr_repository.fluigicloud_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository" "raven_repo" {
+  name                 = "raven"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "gjohnson"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "raven_repo_policy" {
+  repository = aws_ecr_repository.raven_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository" "clothov3_repo" {
+  name                 = "clothov3"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "gjohnson"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "clothov3_repo_policy" {
+  repository = aws_ecr_repository.clothov3_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_ecr_repository" "cellov1_repo" {
+  name                 = "cellov1"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "gjohnson"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "cellov1_repo_policy" {
+  repository = aws_ecr_repository.cellov1_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository" "fpselection_repo" {
+  name                 = "fpselection"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "gjohnson"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "fpselection_repo_policy" {
+  repository = aws_ecr_repository.fpselection_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
